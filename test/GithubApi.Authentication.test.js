13e17d4b26f268e0159e4f87af171a1a6547d4be
Julian O <juospinam@unal.edu.co>
2022-06-28 21:33:40 -0500

    two functions

--- test/GithubApi.Authentication.test.js
+++ test/GithubApi.Authentication.test.js
@@ -14,16 +14,14 @@ describe('Github Api Test', () => {
           Authorization: `token ${process.env.ACCESS_TOKEN}`
         }
       });
-      it('Via OAuth2 Tokens by parameter', async () => {
-        const response2 = await axios.get(
-          `${urlBase}/repos/${githubUserName}/${repository}`,
-          { access_token: process.env.ACCESS_TOKEN }
-        );
-
-        expect(response2.status).to.equal(StatusCodes.OK);
-        expect(response2.data.description).equal('This is a Workshop about Api Testing in JavaScript');
-      });
-
+      expect(response.status).to.equal(StatusCodes.OK);
+      expect(response.data.description).equal('This is a Workshop about Api Testing in JavaScript');
+    });
+    it('Via OAuth2 Tokens by parameter', async () => {
+      const response = await axios.get(
+        `${urlBase}/repos/${githubUserName}/${repository}`,
+        { access_token: process.env.ACCESS_TOKEN }
+      );
       expect(response.status).to.equal(StatusCodes.OK);
       expect(response.data.description).equal('This is a Workshop about Api Testing in JavaScript');
     });
