391a988bd4b654bb540132c78d4d9079e119fed4
Julian O <juospinam@unal.edu.co>
2022-07-03 15:45:46 -0500

    test idempotence

--- test/GithubApi.Put.test.js
+++ test/GithubApi.Put.test.js
@@ -24,7 +24,23 @@ describe('Github Api Test 3', () => {
     aperdomob is in */
     it('Follower test', async () => {
       const response = await object.get('https://api.github.com/user/following');
-      expect(response.data[0].login).to.eql('aperdomob');
+      expect(response.data.find((user) => user.login === 'aperdomob')).to.not.eql(undefined);
+    });
+  });
+  describe('Testing idempotence', () => {
+    /* Test used for following aperdomo user with the axios put method
+     and check with the status if the response works correctly and if his body
+     is empty */
+    it('PUT method', async () => {
+      const response = await object.put('https://api.github.com/user/following/aperdomob');
+      expect(response.status).to.equal(204);
+      expect(response.data).to.equal('');
+    });
+    /* Test used to verify in the list of people that i follow if
+    aperdomob is in */
+    it('Follower test', async () => {
+      const response = await object.get('https://api.github.com/user/following');
+      expect(response.data.find((user) => user.login === 'aperdomob')).to.not.eql(undefined);
     });
   });
 });
